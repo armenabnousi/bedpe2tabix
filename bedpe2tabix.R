81fed2ab75ce3d4217f2794f9be9cff615da478e
Armen Abnousi <a.abnousi@outlook.com>
2019-04-09 11:15:24 -0400

    initial commit

---
+++ bedpe2tabix.R
@@ -0,0 +1,29 @@
+args <- commandArgs(trailingOnly = TRUE)
+print(args)
+bedpe <- args[1]
+tabix_filename <- args[2]
+#quit()
+d <- read.csv(bedpe, sep = "\t")
+head(d)
+#print(d$score)
+d$score <- -log(as.numeric(as.character(d$fdr)))
+#d <- d[complete.cases(d),]
+#print(d$score)
+d$lr <- paste0(d$chr2, ":", d$start2, "-", d$end2, ",", d$score)
+d$rl <- paste0(d$chr1, ":", d$start1, "-", d$end1, ",", d$score)
+tabixl <- d[,c("chr1", "start1", "end1", "lr")]
+tabixr <- d[,c("chr2", "start2", "end2", "rl")]
+colnames(tabixr) <- colnames(tabixl)
+tabixl$id <- seq(1, dim(tabixl)[1] * 2, 2)
+tabixr$id <- seq(2, dim(tabixr)[1] * 2, 2)
+tabix <- rbind(tabixl, tabixr)
+tabix <- tabix[order(tabix$id),]
+tabix$dir <- "."
+options(scipen = 999)
+print(colnames(tabix))
+tabix <- tabix[order(tabix$chr1, tabix$start1),]
+write.table(tabix, tabix_filename, quote = F, row.names = F, col.names = F, sep = "\t")
+system(paste0("bgzip -c ", tabix_filename, " > ", tabix_filename, ".gz"))
+system(paste0("tabix -p bed ", tabix_filename, ".gz"))
+#quit()
+#write.table(tabix, tabix_filename, quote = F, row.names = F, col.names = F, sep = "\t")
